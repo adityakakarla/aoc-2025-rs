f623b59a26f4806bd4396d699caf02fdc189998b
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2026-01-26 23:40:16 -0500

    brute force day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod solutions;
-use crate::solutions::day_1::solve_day_1;
+use crate::solutions::day_1::{solve_day_1_part_1, solve_day_1_part_2};
 
 fn main() {
-    solve_day_1();
+    // solve_day_1_part_1();
+    solve_day_1_part_2();
 }
--- src/solutions/day_1.rs
+++ src/solutions/day_1.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-pub fn solve_day_1() {
+pub fn solve_day_1_part_1() {
     let text = fs::read_to_string("./data/input/day_1.txt").expect("Could not parse text file");
     let lines = text.lines();
 
@@ -25,5 +25,37 @@ pub fn solve_day_1() {
         }
     }
 
-    println!("{}", zero_count);
+    println!("Day 1 Part 1: {}", zero_count);
+}
+
+pub fn solve_day_1_part_2() {
+    let text = fs::read_to_string("./data/input/day_1.txt").expect("Could not parse text file");
+    let lines = text.lines();
+
+    let mut current_value = 50;
+    let mut zero_count = 0;
+
+    for line in lines {
+        let direction = line.chars().nth(0).expect("Could not get direction char");
+        let steps_str = &line[1..line.len()];
+        let num_steps: i32 = steps_str.parse().expect("Could not get number of steps");
+        let num_direction;
+
+        if direction == 'L' {
+            num_direction = -1;
+        } else if direction == 'R' {
+            num_direction = 1;
+        } else {
+            panic!("No valid direction passed");
+        }
+
+        for _ in 0..num_steps {
+            current_value += num_direction;
+            if (current_value % 100) == 0 {
+                zero_count += 1;
+            }
+        }
+    }
+
+    println!("Day 1 Part 2: {}", zero_count);
 }
