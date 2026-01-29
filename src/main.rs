0e69f0e3c40c914a48c8ae344fb80f0cc6821c3f
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2026-01-28 21:27:25 -0500

    complete day 3

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 mod solutions;
-use crate::solutions::day_3::solve_day_3_part_1;
+use crate::solutions::{
+    day_1::{solve_day_1_part_1, solve_day_1_part_2},
+    day_2::{solve_day_2_part_1, solve_day_2_part_2},
+    day_3::{solve_day_3_part_1, solve_day_3_part_2},
+};
 
 fn main() {
-    // solve_day_1_part_1();
-    // solve_day_1_part_2();
-    // solve_day_2_part_1();
-    // solve_day_2_part_2();
+    solve_day_1_part_1();
+    solve_day_1_part_2();
+    solve_day_2_part_1();
+    solve_day_2_part_2();
     solve_day_3_part_1();
+    solve_day_3_part_2();
 }
