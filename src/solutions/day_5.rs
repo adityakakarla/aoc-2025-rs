4097f28efc6bc1a98b1aea62a82cd2087621f972
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2026-01-30 08:03:33 -0500

    solveed day 5 part 2

--- src/solutions/day_5.rs
+++ src/solutions/day_5.rs
@@ -1,4 +1,4 @@
-use std::fs::read_to_string;
+use std::{cmp::max, fs::read_to_string};
 
 pub fn solve_day_5_part_1() {
     let text = read_to_string("./data/input/day_5.txt").expect("Could not parse text file");
@@ -44,10 +44,63 @@ fn part_1(text: String) -> i32 {
     fresh_count
 }
 
-pub fn solve_day_5_part_2() {}
+pub fn solve_day_5_part_2() {
+    let text = read_to_string("./data/input/day_5.txt").expect("Could not parse text file");
+    let result = part_2(text);
+    println!("Day 5 Part 2: {}", result);
+}
 
-fn part_2(text: String) -> i32 {
-    0
+fn part_2(text: String) -> i64 {
+    let mut ranges = Vec::new();
+    let mut fresh_count = 0;
+
+    for line in text.trim().lines() {
+        if line.trim().is_empty() {
+            break;
+        }
+
+        let mut line_split = line.trim().split("-");
+
+        let start: i64 = line_split
+            .next()
+            .unwrap()
+            .parse()
+            .expect("Could not parse start");
+        let end: i64 = line_split
+            .next()
+            .unwrap()
+            .parse()
+            .expect("Could not parse end");
+
+        ranges.push((start, end));
+    }
+
+    ranges.sort();
+
+    let mut new_ranges = Vec::new();
+
+    for (start, end) in ranges {
+        if new_ranges.len() == 0 {
+            new_ranges.push(vec![start, end]);
+            continue;
+        }
+
+        let new_range_count = new_ranges.len();
+
+        if start <= new_ranges[new_range_count - 1][1] {
+            new_ranges[new_range_count - 1][1] = max(new_ranges[new_range_count - 1][1], end);
+        } else {
+            new_ranges.push(vec![start, end]);
+        }
+    }
+
+    for range in new_ranges {
+        let start = range[0];
+        let end = range[1];
+        fresh_count += end as i64 - start as i64 + 1;
+    }
+
+    fresh_count
 }
 
 #[cfg(test)]
@@ -69,5 +122,10 @@ mod tests {
     #[test]
     fn test_part_1() {
         assert_eq!(part_1(TEST_INPUT.to_string()), 3);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(TEST_INPUT.to_string()), 14);
     }
 }
