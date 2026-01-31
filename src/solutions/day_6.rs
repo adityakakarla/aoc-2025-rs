d02eb50c8142509adc5b112ef206e66bd898ad28
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2026-01-31 18:26:41 -0500

    solved day 6 part 2, a problem I spent way too long on because I did not realise that the auto-formatter in my ide was removing every trailing space

--- src/solutions/day_6.rs
+++ src/solutions/day_6.rs
@@ -45,28 +45,93 @@ fn part_1(text: String) -> i64 {
     total_sum
 }
 
-pub fn solve_day_6_part_2() {}
+pub fn solve_day_6_part_2() {
+    let text = read_to_string("./data/input/day_6.txt").expect("Could not parse text file");
+    let result = part_2(text);
+    println!("Day 6 Part 2: {}", result);
+}
 
 fn part_2(text: String) -> i64 {
-    0
+    let mut data: Vec<Vec<char>> = Vec::new();
+
+    for line in text.trim().lines() {
+        data.push(line.chars().collect());
+    }
+
+    let mut values = Vec::new();
+    let mut operation = '\0';
+    let mut total_sum = 0;
+
+    for idx in 0..data[0].len() {
+        let mut digits = Vec::new();
+
+        for (line_idx, line) in data.iter().enumerate() {
+            if line_idx == data.len() - 1 {
+                if operation == '\0' {
+                    operation = line[idx]
+                }
+                continue;
+            }
+
+            if line[idx] != ' ' {
+                let digit = line[idx].to_digit(10).unwrap();
+                digits.push(digit);
+            }
+        }
+
+        if digits.len() == 0 || idx == data[0].len() - 1 {
+            if digits.len() > 0 {
+                let value = digits.iter().fold(0, |acc, &x| acc * 10 + x);
+                values.push(value);
+            }
+
+            if operation == '+' {
+                let mut curr_sum = 0;
+                for value in values {
+                    curr_sum += value as i64;
+                }
+                total_sum += curr_sum
+            } else {
+                let mut curr_product = 1;
+                for value in values {
+                    curr_product *= value as i64;
+                }
+                total_sum += curr_product;
+            }
+            values = Vec::new();
+            operation = '\0';
+        } else {
+            let value = digits.iter().fold(0, |acc, &x| acc * 10 + x);
+            values.push(value);
+        }
+    }
+
+    total_sum
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const TEST_INPUT: &str = "
-    123 328  51 64
-     45 64  387 23
-      6 98  215 314
-    *   +   *   +
-    ";
+    const TEST_INPUT: &str = "123 328  51 64
+ 45 64  387 23
+  6 98  215 314
+*   +   *   +  ";
 
     #[test]
     fn test_part_1() {
         assert_eq!(part_1(TEST_INPUT.to_string()), 4277556);
     }
 
+    // NOTE:
+    // Auto-formatting made this way more complicated.
+    // Make sure text is pasted without auto-formatting, otherwise
+    // trailing spaces will be removed which is a big deal.
+
     #[test]
-    fn test_part_2() {}
+    fn test_part_2() {
+        let text =
+            read_to_string("./data/input/day_6_test.txt").expect("Could not parse text file");
+        assert_eq!(part_2(text), 3263827);
+    }
 }
